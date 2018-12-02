9ef33c1bf3ec385be36acc6b1b9885186db5b868
Jeevan Prakash <jeevan@prodios.com>
2018-12-02 20:50:53 +0530

    Updated ui and images

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 // setup public folder as root
 app.use(express.static("public"));
